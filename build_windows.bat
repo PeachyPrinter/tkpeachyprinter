bb469406caae1df3d0adff861f4db19c9555c13e
James Townley <software@peachyprinter.com>
2015-02-27 12:20:20 -0600

    Adding versioning

--- build_windows.bat
+++ build_windows.bat
@@ -1,3 +1,46 @@
+@echo off
+
+echo ------------------------------------
+echo Cleaning workspace
+echo ------------------------------------
+
+del /Q *.msi
+rmdir /S /Q src\build
+rmdir /S /Q src\dist
+REM TODO JT 2014-02-04 - Should clean the workspace
+
+echo ------------------------------------
+echo Extracting Git Revision Number
+echo ------------------------------------
+
+set SEMANTIC=0.0.1
+set /p SEMANTIC=<symantic.version
+IF NOT DEFINED GIT_HOME (
+  git --version
+  IF "%ERRORLEVEL%" == "0" (
+    set GIT_HOME=git
+  ) ELSE (
+    echo "Could not find git."
+    pause
+    EXIT /B 1
+  )
+)
+
+for /f "delims=" %%A in ('%GIT_HOME% rev-list HEAD --count') do set "GIT_REV_COUNT=%%A"
+for /f "delims=" %%A in ('%GIT_HOME% rev-parse HEAD') do set "GIT_REV=%%A"
+
+set VERSION=%SEMANTIC%.%GIT_REV_COUNT%
+echo Version: %VERSION%
+echo # THIS IS A GENERATED FILE  > version.properties
+echo version='%VERSION%' >> version.properties
+echo revision='%GIT_REV%' >> version.properties
+echo Git Revision Number is %GIT_REV_COUNT%
+copy version.properties src\VERSION.py
+
+echo ------------------------------------
+echo Creating Package
+echo ------------------------------------
+
 cd src
 python setup.py bdist_msi
 IF NOT "%ERRORLEVEL%" == "0" (
