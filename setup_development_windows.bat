7e5aac4398d1b1590f8fef1b1e4af5cb279d4c55
James Townley <software@peachyprinter.com>
2015-03-02 12:21:21 -0600

    Got a working setup script

--- setup_development_windows.bat
+++ setup_development_windows.bat
@@ -1,3 +1,4 @@
+@ECHO OFF
 SET will_fail=0
 SET fail_reasons=""
 
@@ -9,7 +10,7 @@ IF NOT "%VIRTUAL_ENV%" == "" (
 )
 
 ECHO "----Checking for Pip----"
-where pip
+where pip 2> nul > nul
 IF NOT "%ERRORLEVEL%" == "0" (
     ECHO "FAILURE: PIP is not available, you should install it."
     SET will_fail=11
@@ -18,7 +19,7 @@ IF NOT "%ERRORLEVEL%" == "0" (
 )
 
 ECHO "----Checking for virtualenv----"
-where virtualenv
+where virtualenv 2> nul > nul
 IF NOT "%ERRORLEVEL%" == "0" (
     ECHO "FAILURE: virtualenv not available, you should install it."
     SET will_fail=12
@@ -27,58 +28,64 @@ IF NOT "%ERRORLEVEL%" == "0" (
 )
 
 ECHO "----Checking for and create a virtual environment----"
-create_venv="TRUE"
-IF EXISTS venv (
-    :question
-    SET /P anwser "Do you wish remove and re-install this environment? (y/n)"
-    IF /i {%anwser%}=={y} (GOTO :yes) 
-    IF /i {%anwser%}=={n} (GOTO :no) 
-    GOTO :question 
+SET create_venv="TRUE"
+IF NOT EXIST "venv" GOTO endquesiton
+:question
+SET /P anwser="Do you wish remove and re-install this environment? (y/n)"
+IF /i {%anwser%}=={y} (GOTO :yes) 
+IF /i {%anwser%}=={n} (GOTO :no) 
+GOTO :question 
 
-    :yes 
-    create_venv="TRUE"
-    GOTO :endquesiton
-    
-    :no 
-    create_venv="FALSE"
-    GOTO :endquesiton
+:yes 
+SET create_venv="TRUE"
+GOTO :endquesiton
 
-    :endquesiton
-)
+:no 
+SET create_venv="FALSE"
+GOTO :endquesiton
+
+:endquesiton
+
 
 IF %create_venv% == "TRUE" (
-    virtualenv venv
+    ECHO "--------Createing new virtualenv----"
+    ECHO "--------Note for windows to ease use system packages are included ----"
+    ECHO "--------To avoid this and build from a clean environment use --no-site-packages -----"
+    virtualenv --system-site-packages  venv 
     IF NOT "%ERRORLEVEL%" == "0" (
         ECHO "FAILURE: Virutal environment creation failed"
         SET will_fail=59
         SET fail_reasons="%fail_reasons%\nFAILURE: Virutal environment creation failed"
         EXIT /B %will_fail%
+    )
 )
 
-source venv/bin/activate
-IF "%VIRTUAL_ENV%" == ""(
-ECHO "FAILURE: Virutal environment activation failed"
+ECHO "--------Activating virtualenv----"
+echo on
+CALL "venv\Scripts\activate.bat"
+IF "%VIRTUAL_ENV%" == "" (
+        ECHO "FAILURE: Virutal environment activation failed"
         SET will_fail=666
         SET fail_reasons="%fail_reasons%\nFAILURE: Virutal environment activation failed"
         EXIT /B %will_fail%
-
+)
 
 ECHO "----Setting up virtual environment----"
 
 ECHO "--------Setting up numpy----"
-python -c"import numpy"
+python -c"import numpy" 2> nul > nul
 IF NOT "%ERRORLEVEL%" == "0" (
     ECHO "Numpy not available adding"
     pip install -U --force numpy
     IF NOT "%ERRORLEVEL%" == "0" (
-        ECHO "FAILURE: Numpy failed installing"
+        ECHO "FAILURE: Numpy failed installing, you can manually install it from http://sourceforge.net/projects/numpy/"
         SET will_fail=1
-        fail_reasons="%fail_reasons%\nFAILURE: Numpy failed installing"
+        fail_reasons="%fail_reasons%\nFAILURE: Numpy failed installing, you can manually install it from http://sourceforge.net/projects/numpy/"
     )
 )
 
 ECHO "--------Setting up cx_Freeze----"
-python -c"import cx_Freeze"
+python -c"import cx_Freeze" 2> nul > nul
 IF NOT "%ERRORLEVEL%" == "0" (
     ECHO "cx-freeze Required installing"
     pip install -U cx_Freeze
@@ -90,21 +97,20 @@ IF NOT "%ERRORLEVEL%" == "0" (
 )
 
 ECHO "--------Setting up pyaudio----"
-python -c"import pyaudio"
+python -c"import pyaudio" 2> nul > nul
 IF NOT "%ERRORLEVEL%" == "0" (
     ECHO "PyAudio Required, you can manually install it from: http://people.csail.mit.edu/hubert/pyaudio/#downloads"
     SET will_fail=4
     SET fail_reasons="%fail_reasons%\nFAILURE :PyAudio Required, you can manually install it from: http://people.csail.mit.edu/hubert/pyaudio/#downloads"
-    )
 )
 
-IF [ %will_fail% != 0 ]; then
-    ECHO "Enviroment SETup failed"
+IF NOT %will_fail% == 0 (
+    ECHO "Enviroment Setup failed"
     ECHO -e %fail_reasons%
-    exit %will_fail%
+    EXIT /B %will_fail%
 )
 
 ECHO ""
 ECHO "-----------------------------------"
-ECHO "Enviroment SETup complete and seemingly successful."
+ECHO "Enviroment Setup complete and seemingly successful."
 ECHO "You can start the enviroment with the command\"source venv/bin/activate\""
