ee4e0ae81ecc73b2d8b962296eb8e14443f8a342
James Townley <software@peachyprinter.com>
2015-03-02 10:32:43 -0600

    First pass at windows setup batch script

--- setup_development_windows.bat
+++ setup_development_windows.bat
@@ -1,131 +1,110 @@
-#!/bin/bash
-echo "SO NOT WORKING AT ALL"
-exit
+SET will_fail=0
+SET fail_reasons=""
 
-echo "----Checking for already running Virtual Environment----"
-if [[ "$VIRTUAL_ENV" != "" ]]; then
-    echo "Deactivitate the existing virtual enviroment before running this script."
-    echo "This can be done with the \"deactivate\" command."
-    exit 53 
-fi
+ECHO "----Checking for already running Virtual Environment----"
+IF NOT "%VIRTUAL_ENV%" == "" (
+    ECHO "Deactivitate the existing virtual enviroment before running this script."
+    ECHO "This can be done with the \"deactivate\" command."
+    EXIT /B 53 
+)
 
-echo "----Checking for Pip----"
-command -v pip 2>&1 >/dev/null
-if [ $? != 0 ]; then
-    echo "Pip not available, you should be prompted for install:"
-    sudo apt-get install python-pip
-    if [ $? != 0 ]; then
-        echo "FAILURE: Pip failed installing"
-        WILL_FAIL=11
-        FAIL_REASONS="$FAIL_REASONS\nFAILURE: Pip failed installing"
-    fi
-fi
+ECHO "----Checking for Pip----"
+where pip
+IF NOT "%ERRORLEVEL%" == "0" (
+    ECHO "FAILURE: PIP is not available, you should install it."
+    SET will_fail=11
+    SET fail_reasons="%fail_reasons%\nFAILURE: Pip failed installing"
+    EXIT /B %will_fail%
+)
 
+ECHO "----Checking for virtualenv----"
+where virtualenv
+IF NOT "%ERRORLEVEL%" == "0" (
+    ECHO "FAILURE: virtualenv not available, you should install it."
+    SET will_fail=12
+    SET fail_reasons="%fail_reasons%\nFAILURE: virtualenv not available, you should install it."
+    EXIT /B %will_fail%
+)
 
-echo "----Checking for virtualenv----"
-command -v virtualenv 2>&1 >/dev/null
-if [ $? != 0 ]; then
-    echo "virtualenv not available, you should be prompted for install:"
-    sudo apt-get install python-virtualenv
-    if [ $? != 0 ]; then
-        echo "FAILURE: virtualenv failed installing"
-        WILL_FAIL=12
-        FAIL_REASONS="$FAIL_REASONS\nFAILURE: virtualenv failed installing"
-    fi
-fi
+ECHO "----Checking for and create a virtual environment----"
+create_venv="TRUE"
+IF EXISTS venv (
+    :question
+    SET /P anwser "Do you wish remove and re-install this environment? (y/n)"
+    IF /i {%anwser%}=={y} (GOTO :yes) 
+    IF /i {%anwser%}=={n} (GOTO :no) 
+    GOTO :question 
 
+    :yes 
+    create_venv="TRUE"
+    GOTO :endquesiton
+    
+    :no 
+    create_venv="FALSE"
+    GOTO :endquesiton
 
-echo "----Checking for and create a virtual environment----"
-CREATE_VENV="TRUE"
-if [ -d "venv" ]; then
-    while true; do
-    read -p "Do you wish remove and re-install this environment?" yn
-    case $yn in
-        [Yy]* ) rm -rf venv && CREATE_VENV="TRUE"; break;;
-        [Nn]* ) CREATE_VENV="FALSE"; break;;
-        * ) echo "Please answer yes or no.";;
-    esac
-    done
-fi
-if [ $CREATE_VENV == "TRUE" ]; then
+    :endquesiton
+)
+
+IF %create_venv% == "TRUE" (
     virtualenv venv
-    if [ $? != 0 ]; then
-        echo "Virutal environment failed"
-        exit 59
-    fi
-fi
+    IF NOT "%ERRORLEVEL%" == "0" (
+        ECHO "FAILURE: Virutal environment creation failed"
+        SET will_fail=59
+        SET fail_reasons="%fail_reasons%\nFAILURE: Virutal environment creation failed"
+        EXIT /B %will_fail%
+)
+
 source venv/bin/activate
-if [[ "$VIRTUAL_ENV" == "" ]]; then
-    echo "FAILURE: Virutal environment creation failed"
-    exit 666
-fi
+IF "%VIRTUAL_ENV%" == ""(
+ECHO "FAILURE: Virutal environment activation failed"
+        SET will_fail=666
+        SET fail_reasons="%fail_reasons%\nFAILURE: Virutal environment activation failed"
+        EXIT /B %will_fail%
 
-echo "----Setting up virtual environment----"
-SETUP_TMP="setup_tmp"
-WILL_FAIL=0
-FAIL_REASONS=""
 
-echo "--------Setting up numpy----"
-python -c"import numpy" 2>&1 >/dev/null
-if [ $? != 0 ]; then
-    echo "Numpy not available adding"
+ECHO "----Setting up virtual environment----"
+
+ECHO "--------Setting up numpy----"
+python -c"import numpy"
+IF NOT "%ERRORLEVEL%" == "0" (
+    ECHO "Numpy not available adding"
     pip install -U --force numpy
-    if [ $? != 0 ]; then
-        echo "FAILURE: Numpy failed installing"
-        WILL_FAIL=1
-        FAIL_REASONS="$FAIL_REASONS\nFAILURE: Numpy failed installing"
-    fi
-fi
+    IF NOT "%ERRORLEVEL%" == "0" (
+        ECHO "FAILURE: Numpy failed installing"
+        SET will_fail=1
+        fail_reasons="%fail_reasons%\nFAILURE: Numpy failed installing"
+    )
+)
 
-echo "--------Setting up cx_Freeze----"
-dpkg --get-selections | grep -v deinstall | grep cx-freeze 2>&1 >/dev/null
-if [ $? != 0 ]; then
-    echo "cx-freeze Required"
-    echo "You should be prompted to install via apt-get now"
-    sudo apt-get install cx-freeze
-    if [ $? != 0 ]; then
-        echo "FAILURE: cx-freeze failed installing"
-        WILL_FAIL=2
-        FAIL_REASONS="$FAIL_REASONS\nFAILURE: cx-freeze failed installing (APT)"
-    fi
-fi
+ECHO "--------Setting up cx_Freeze----"
+python -c"import cx_Freeze"
+IF NOT "%ERRORLEVEL%" == "0" (
+    ECHO "cx-freeze Required installing"
+    pip install -U cx_Freeze
+    IF NOT "%ERRORLEVEL%" == "0" (
+        ECHO "FAILURE: cx-freeze failed installing, You can manually install it from: https://pypi.python.org/pypi?:action=display&name=cx_Freeze&version=4.3.4 "
+        SET will_fail=2
+        SET fail_reasons="%fail_reasons%\nFAILURE: cx-freeze failed installing, You can manually install it from: https://pypi.python.org/pypi?:action=display&name=cx_Freeze&version=4.3.4"
+    )
+)
 
-ln -s /usr/lib/pymodules/python2.7/cx_Freeze venv/lib/python2.7/site-packages/cx_Freeze
-ln -s /usr/lib/pymodules/python2.7/cx_Freeze-4.3.1.egg-info venv/lib/python2.7/site-packages/cx_Freeze-4.3.1.egg-info
-python -c"import cx_Freeze" 2>&1 >/dev/null
-if [ $? != 0 ]; then
-        WILL_FAIL=3
-        FAIL_REASONS="$FAIL_REASONS\nFAILURE: cx_Freeze failed installing (LINK)"
-fi
+ECHO "--------Setting up pyaudio----"
+python -c"import pyaudio"
+IF NOT "%ERRORLEVEL%" == "0" (
+    ECHO "PyAudio Required, you can manually install it from: http://people.csail.mit.edu/hubert/pyaudio/#downloads"
+    SET will_fail=4
+    SET fail_reasons="%fail_reasons%\nFAILURE :PyAudio Required, you can manually install it from: http://people.csail.mit.edu/hubert/pyaudio/#downloads"
+    )
+)
 
-echo "--------Setting up pyaudio----"
-dpkg --get-selections | grep -v deinstall | grep python-pyaudio 2>&1 >/dev/null
-if [ $? != 0 ]; then
-    echo "PyAudio Required"
-    echo "You should be prompted to install via apt-get now"
-    sudo apt-get install python-pyaudio
-    if [ $? != 0 ]; then
-        WILL_FAIL=4
-        FAIL_REASONS="$FAIL_REASONS\nFAILURE: pyaudio failed installing (APT)"
-    fi
-fi
+IF [ %will_fail% != 0 ]; then
+    ECHO "Enviroment SETup failed"
+    ECHO -e %fail_reasons%
+    exit %will_fail%
+)
 
-ln -fs /usr/lib/python2.7/dist-packages/pyaudio.py venv/lib/python2.7/site-packages/pyaudio.py
-ln -fs /usr/lib/python2.7/dist-packages/_portaudio.so venv/lib/python2.7/site-packages/_portaudio.so
-ln -fs /usr/lib/python2.7/dist-packages/PyAudio-0.2.8.egg-info venv/lib/python2.7/site-packages/PyAudio-0.2.8.egg-info
-python -c"import pyaudio" 2>&1 >/dev/null
-if [ $? != 0 ]; then
-        WILL_FAIL=5
-        FAIL_REASONS="$FAIL_REASONS\nFAILURE: pyaudio failed installing (LINK)"
-fi
-
-if [ $WILL_FAIL != 0 ]; then
-    echo "Enviroment setup failed"
-    echo -e $FAIL_REASONS
-    exit $WILL_FAIL
-fi
-
-echo ""
-echo "-----------------------------------"
-echo "Enviroment setup complete and seemingly successful."
-echo "You can start the enviroment with the command\"source venv/bin/activate\""
+ECHO ""
+ECHO "-----------------------------------"
+ECHO "Enviroment SETup complete and seemingly successful."
+ECHO "You can start the enviroment with the command\"source venv/bin/activate\""
